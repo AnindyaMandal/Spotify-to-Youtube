d01cd2aed7a8f40e2291e695abcaaa8584e5be97
AnindyaMandal <anindya.cpsc@gmail.com>
2023-09-20 00:24:08 -0400

    Got user playlist json data

--- http_server.py
+++ http_server.py
@@ -12,7 +12,7 @@ def get_codestate():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((SERVER_HOST, SERVER_PORT))
-    server_socket.settimeout(5.0)
+    server_socket.settimeout(20.0)
     server_socket.listen(1)
     print('Listening on port %s ...' % SERVER_PORT)
 
--- spotify_data.py
+++ spotify_data.py
@@ -1,4 +1,4 @@
-import os, requests, spotify_oauth
+import os, requests, spotify_oauth, json
 
 
 
@@ -9,6 +9,25 @@ spotify_oauth.auth()
 
 
 base_uri = "https://api.spotify.com/v1"
+profile_uri = 'https://api.spotify.com/v1/me'
+
+def get_all_playlists():
+    access_token = os.environ['SPOTIFY_ACCESS_TOKEN']
+    response = requests.get(profile_uri + "/playlists", headers={
+        'Authorization': 'Bearer ' + access_token
+    })
+
+    # print(response.content.decode("utf-8"))
+
+    data = response.json()
+    print("Length of data: num of playlists i think  " + str(len(data["items"])))
+    print(data["items"][0]["name"])
+
+
+
+
+
+get_all_playlists()
 
 
 
